681d0c349ef942c391c269b0da1b90facd99b975
NickBulbeck <26549198+NickBulbeck@users.noreply.github.com>
2022-01-26 22:45:30 +0000

    Hmm...

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -82,4 +82,18 @@ router.delete('/courses/:id', asyncHandler(async (req,res,next) => {
   // returns 204 status.
 }));
 
-module.exports = router;
+
+
+/*
+const setMyInfo = (text) => {
+  return (req, res, next) => {
+    req.myInfo = text;
+    next();
+  };
+};
+
+app.get("/", setMyInfo("Test-123"), (req, res) => {
+  res.send("Hi there! myInfo = " + req.myInfo);
+});
+
+*/
